c579527e81aaec176eb4a9083f32f1a3d4164f50
Sumon mia <31125805+sumonwd@users.noreply.github.com>
2019-07-21 13:10:35 +0600

    add content ticker

--- elementor_addons/Classes/Include_Admin_Template.php
+++ elementor_addons/Classes/Include_Admin_Template.php
@@ -27,7 +27,7 @@ trait Include_Admin_Template {
                 'icon_box',
                 'call_to_action',
                 'tabs',
-                'info_box',
+                'content_ticker',
                 'dual_color_heading',
                 'tooltip',
                 'advanced_accordion',
