eb744ddcd2470132fcc14f3b0cefa4f917bb3abe
Richardsetu <richardsetu1@gmail.com>
2019-07-30 15:27:24 +0600

    fsd

--- beaver_builder_addons/modules/accordions/includes/frontend.php
+++ beaver_builder_addons/modules/accordions/includes/frontend.php
@@ -3,18 +3,77 @@
 //print_r($settings);
 //echo "</pre>";
 ?>
-
+<style>
+    .SA-FL-accordion-header{
+        width: 100%;
+        float: left;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        border: 1px solid #000;
+        border-radius: 5px;
+        padding: 10px;
+    }
+    .span-deactive{
+            display: flex;
+            float: left;
+            align-items: center;
+            justify-content: center;
+            background: red;
+            font-size: 20px;
+            width: 100%;    
+            max-width: 100px;
+            height: 100px;
+            color: blue;
+            border: 1px solid yellow;
+            border-radius: 5px;
+            margin: 10px;
+            
+        }
+        .SA-FL-accordion-header.active .span-active{
+            display: flex;
+            float: left;
+            align-items: center;
+            justify-content: center;
+             background: red;
+            font-size: 20px;
+            width: 100%;    
+            max-width: 100px;
+            height: 100px;
+            color: blue;
+            border: 1px solid yellow;
+            border-radius: 5px;
+            margin: 10px;
+            
+        }
+        .SA-FL-accordion-header:hover .span-deactive{
+            color: white;
+            border-color: green;
+            background: blue; 
+        }
+        .SA-FL-accordion-header .span-active{
+            display: none;
+        }
+        .SA-FL-accordion-header.active .span-deactive{
+            display: none;
+        }
+</style>
 
 <div class="SA-FL-accordion-main-area">
-    <div class="" ref="">
-        <div class="span-active">icon one</div>
-        <div class="span-deactive">icon two</div>
-        <div class="heading-data">This is accordion</div>
-    </div>
-    <div class="" id="">
-        <div class="">
-            loreal i's taata taata d'art boa poaceous
-        </div>
-    </div>
+    <?php foreach ($settings->add_accordion as $value) { ?>
+        <div class="SA-FL-accordion-area">
+            <div class="SA-FL-accordion-header" ref="">
+                <div class="span-active">icon one</div>
+                <div class="span-deactive">icon two</div>
+                <div class="heading-data">This is accordion</div>
+            </div>
+            <div class="SA-FL-accordion-details-area" id="">
+                <div class="SA-FL-accordion-details">
+                    loreal i's taata taata d'art boa poaceous
+                </div>
+            </div>
+        </div>  
+
+    <?php } ?>
 
 </div>
