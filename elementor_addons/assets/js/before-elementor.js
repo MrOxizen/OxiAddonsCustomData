e552330efe3d3846e2ee0996d0ceac10b3c487e1
Sumon mia <31125805+sumonwd@users.noreply.github.com>
2019-07-30 16:14:58 +0600

    fix js

--- elementor_addons/assets/js/before-elementor.js
+++ elementor_addons/assets/js/before-elementor.js
@@ -6,8 +6,6 @@
             n("#elementor-panel-elements-search-input").on("keyup", _.debounce(function () {
                 n("#elementor-panel-elements").find(".oxi-el-admin-icon").parents(".elementor-element").addClass("sa-el-widgets-addons")
             }, 100))
-        }), e.hooks.addAction("panel/open_editor/widget", function (e, n) {
-            l(n)
         })
     }(elementor, jQuery, window);
 }, 1000);
