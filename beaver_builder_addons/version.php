f623d860e8d5590ff2fd662fcdc1284bd374e062
MrOxizen <50793821+MrOxizen@users.noreply.github.com>
2019-07-31 13:33:20 +0600

    Update version.php

--- beaver_builder_addons/version.php
+++ beaver_builder_addons/version.php
@@ -1,7 +1,8 @@
 <?php
 /*
   Plugin Name: Shortcode Addons
-  Version: 1.6
+  Version: 1.6\
+ * https://github.com/MrOxizen/Shortcode-Addons
  */
 
 return array(1.0, 'Beaver Builder', true, 'oxi-addons-fl-builder');
