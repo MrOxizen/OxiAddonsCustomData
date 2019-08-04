daed8aafb77c06d98f286149e0d3542f1543c2cd
Richardsetu <richardsetu1@gmail.com>
2019-08-04 15:10:23 +0600

    sdffds

--- beaver_builder_addons/modules/sa-offcanvas/includes/frontend.php
+++ beaver_builder_addons/modules/sa-offcanvas/includes/frontend.php
@@ -6,15 +6,87 @@
 //print_r(jQuerysettings);
 //echo '</pre>';
 ?>
+<script>
+    jQuery(function () {
 
+        jQuery(document).on("beforecreate.offcanvas", function (e) {
+            var dataOffcanvas = jQuery(e.target).data('offcanvas-component');
+            console.log(dataOffcanvas);
+            dataOffcanvas.onInit = function () {
+                console.log(this);
+            };
+        });
+
+        jQuery(document).on("create.offcanvas", function (e) {
+            var dataOffcanvas = jQuery(e.target).data('offcanvas-component');
+            console.log(dataOffcanvas.options);
+            dataOffcanvas.onOpen = function () {
+                console.log('Callback onOpen');
+            };
+            dataOffcanvas.onClose = function () {
+                console.log('Callback onClose');
+            };
+        });
+
+        jQuery(document).on("clicked.offcanvas-trigger clicked.offcanvas", function (e) {
+            var dataBtnText = jQuery(e.target).text();
+            console.log(e.type + '.' + e.namespace + ': ' + dataBtnText);
+        });
+
+        jQuery(document).on("open.offcanvas", function (e) {
+            var dataOffcanvasID = jQuery(e.target).attr('id');
+            console.log(e.type + ': #' + dataOffcanvasID);
+        });
+
+        jQuery(document).on("resizing.offcanvas", function (e) {
+            var dataOffcanvasID = jQuery(e.target).attr('id');
+            console.log(e.type + ': #' + dataOffcanvasID);
+        });
+
+        jQuery(document).on("close.offcanvas", function (e) {
+            var dataOffcanvasID = jQuery(e.target).attr('id');
+            console.log(e.type + ': #' + dataOffcanvasID);
+        });
+
+        jQuery(document).on("destroy.offcanvas", function (e) {
+            var dataOffcanvasID = jQuery(e.target).attr('id');
+            console.log(e.type + ': #' + dataOffcanvasID);
+        });
+
+        jQuery('#bottom').on("create.offcanvas", function (e) {
+            var api = jQuery(this).data('offcanvas-component');
+
+            console.log(api);
+            jQuery('.sa-destroy').on('click', function () {
+                api.destroy();
+                //jQuery( '#top' ).data('offcanvas-component').destroy();
+                console.log(api);
+                console.log(jQuery('#top').data());
+            });
+        });
+
+        jQuery('#left').offcanvas({
+
+            modifiers: "left,overlay",
+            triggerButton: '.sa-offcanvas-trigger-left'
+        });
+
+        jQuery('.sa-enhance').on('click', function () {
+            console.log('enhance');
+            jQuery(document).trigger("enhance");
+        });
+
+        jQuery(document).trigger("enhance");
+    });
+</script>
 
 <div clas="oxi_sa_offcanvas_wrapter">
     <main class="c-offcanvas-content-wrap" role="main">
         <div class="o-wrapper">
-            <a class="sa-offcanvas-trigger-left" href="#left" >Left</a>
-            <a class="sa-offcanvas-trigger" data-offcanvas-trigger="right" href="#right">Right</a>
-            <a class="sa-offcanvas-trigger" data-offcanvas-trigger="top" href="#top">Top</a>
-            <a class="sa-offcanvas-trigger" data-offcanvas-trigger="bottom" href="#bottom">Bottom</a>
+            <div class="sa-offcanvas-trigger-left" data-offcanvas-trigger="left"   >Left</div>
+            <a class="sa-offcanvas-trigger-right" data-offcanvas-trigger="right" >Right</a>
+            <a class="sa-offcanvas-trigger" data-offcanvas-trigger="top" >Top</a>
+            <a class="sa-offcanvas-trigger" data-offcanvas-trigger="bottom" >Bottom</a>
         </div>
     </main>
 
