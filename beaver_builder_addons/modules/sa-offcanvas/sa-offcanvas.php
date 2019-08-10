bb4abab60870d765772ac452fc92e6853287321a
MrOxizen <50793821+MrOxizen@users.noreply.github.com>
2019-08-10 17:09:22 +0600

    df

--- beaver_builder_addons/modules/sa-offcanvas/sa-offcanvas.php
+++ beaver_builder_addons/modules/sa-offcanvas/sa-offcanvas.php
@@ -1,10 +1,8 @@
 <?php
 
-class SA_OffCanvas extends FLBuilderModule
-{
+class SA_OffCanvas extends FLBuilderModule {
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct(array(
             'name' => __('OffCanvas', SA_FLBUILDER_TEXTDOMAIN),
             'description' => __('A totally shortcode addons element', SA_FLBUILDER_TEXTDOMAIN),
@@ -25,10 +23,11 @@ class SA_OffCanvas extends FLBuilderModule
         //        $this->add_css('font-awesome');
         //        $this->add_js('jquery-bxslider');
         //        // Register and enqueue your own
-                $this->add_css("offcanvas_css", content_url('uploads/OxiAddonsCustomData/beaver_builder_addons/modules/sa-offcanvas/css/js-offcanvas.css', __FILE__));
-                $this->add_js("offcanvas_js", content_url('uploads/OxiAddonsCustomData/beaver_builder_addons/modules/sa-offcanvas/js/js-offcanvas.pkgd.js', __FILE__), 'jquery', '', true);
-//                $this->add_js("offcanvas_js", FL_MODULE_SA_FLBUILDER_URL . 'modules//js/js-offcanvas.pkgd.js', array(), '', true);
+        //      $this->add_css("offcanvas_css", content_url('uploads/OxiAddonsCustomData/beaver_builder_addons/modules/sa-offcanvas/css/js-offcanvas.css', __FILE__));
+        //     $this->add_js("offcanvas_js", content_url('uploads/OxiAddonsCustomData/beaver_builder_addons/modules/sa-offcanvas/js/js-offcanvas.pkgd.js', __FILE__), 'jquery', '', true);
+        //      $this->add_js("offcanvas_js", FL_MODULE_SA_FLBUILDER_URL . 'modules//js/js-offcanvas.pkgd.js', array(), '', true);
     }
+
 }
 
 require_once FL_MODULE_SA_FLBUILDER_URL . 'modules/sa-offcanvas/offcanvas-register.php';
