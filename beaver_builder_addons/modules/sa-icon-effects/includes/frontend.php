108c1955e8e4f136ecee2f74dcf4d25cfb368bc9
alaminpro <35292883+alaminpro@users.noreply.github.com>
2019-08-21 10:38:38 +0600

    Update frontend.php

--- beaver_builder_addons/modules/sa-icon-effects/includes/frontend.php
+++ beaver_builder_addons/modules/sa-icon-effects/includes/frontend.php
@@ -13,12 +13,12 @@ $icon_link = '';
 if ($settings->info_link_type != 'no') {
     if ($settings->icon_link != '') {
         $icon_link = '<a href="' . $settings->icon_link . '" class="oxi__addons_icon" target="' . $settings->icon_link_target . '"  ' . SA_FLBUILDER_HELPER::Sa_fl_builder_get_link_rel($settings->icon_link_target, $settings->icon_link_nofollow, 1) . ' >
-            <i class="fab fa-snapchat-square oxi__icons"></i>
+            <i class="' . $settings->icon_main . ' oxi__icons"></i>
         </a>';
     }
 } else {
     $icon_link = '<div class="oxi__addons_icon">
-            <i class="fab fa-snapchat-square oxi__icons"></i>
+            <i class="' . $settings->icon_main . ' oxi__icons"></i>
         </div>';
 }
 ?>
